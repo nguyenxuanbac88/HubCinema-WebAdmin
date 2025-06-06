f55f4db4f11ab09fb9aaacbf6dcdf1b31524230e
nguyenxuanbac88 <xuanbacatym@gmail.com>
2025-06-06 17:28:59 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HubCinemaAdmin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

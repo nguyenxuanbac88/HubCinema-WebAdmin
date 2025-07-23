2e771bd13661554c23ff495adaa073e8dc10b48b
nguyenxuanbac88 <xuanbacatym@gmail.com>
2025-07-24 05:42:44 +0700

    -View invoice, detail invoice

---
+++ Controllers/InvoiceController.cs
@@ -0,0 +1,35 @@
+﻿using HubCinemaAdmin.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace HubCinemaAdmin.Controllers
+{
+    public class InvoiceController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IConfiguration _configuration;
+
+        public InvoiceController(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+        {
+            _httpClientFactory = httpClientFactory;
+            _configuration = configuration;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var apiUrl = "http://api.dvxuanbac.com:2030/api/Invoice/all";
+
+            var response = await client.GetAsync(apiUrl);
+            if (!response.IsSuccessStatusCode)
+            {
+                TempData["Error"] = "Không thể lấy dữ liệu hóa đơn từ API.";
+                return View(new List<InvoiceAdminViewModel>());
+            }
+
+            var json = await response.Content.ReadAsStringAsync();
+            var invoices = JsonConvert.DeserializeObject<List<InvoiceAdminViewModel>>(json);
+            return View(invoices);
+        }
+    }
+}
---
+++ Models/InvoiceAdminViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace HubCinemaAdmin.Models
+{
+    public class InvoiceAdminViewModel
+    {
+        public int IdInvoice { get; set; }
+        public DateTime CreateAt { get; set; }
+        public int TotalPrice { get; set; }
+        public byte Status { get; set; }
+
+        public string MovieName { get; set; }
+        public DateTime NgayChieu { get; set; }
+        public TimeSpan GioChieu { get; set; }
+        public string Room { get; set; }
+
+        public List<string> Seats { get; set; }
+        public List<FoodItemDto> Foods { get; set; }
+    }
+
+    public class FoodItemDto
+    {
+        public string FoodName { get; set; }
+        public int Quantity { get; set; }
+        public int TotalPrice { get; set; }
+    }
+
+}
